fcb795fd8b9dcd5674255457100fabbf5188fc69
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-11-04 12:53:02 +0100

    added insertion sorting

--- selection_sort.py
+++ selection_sort.py
@@ -23,5 +23,19 @@ def bubble_sort(array: list):
     return array
 
 
-print(selection_sort([1,2,5,3,6,1,8,4]))
-print(bubble_sort([1,2,5,3,6,1,8,4]))
+def insertion_sort(array: list):
+    for i in range(1, len(array)):
+        value = array[i]
+        hole = i
+        while(hole > 0 and array[hole - 1] > value):
+            array[hole] = array[hole - 1]
+            hole -= 1
+
+        array[hole] = value
+
+    return array
+
+
+print(selection_sort([7,1,2,5,3,6,1,8,4]))
+print(bubble_sort([1,2,5,7,3,6,1,8,4]))
+print(insertion_sort([1,2,5,3,7,6,1,8,4]))
